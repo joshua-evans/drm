de8f6f0446bd6d2d4d4880d86673837050901615
joshua-evans <evans.joshua.d@gmail.com>
2025-08-16 13:45:29 -0400

    contact form

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# drm
-# drm
+# durbin Race Management
--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,6 @@
 let carouselHovered = false;
 let pauseCarousel = false;
+let slideIndex = 1;
 
 const showCarouselNavigation = () => {
   const jsCarouselNavigation = document.getElementById("js_nav");
@@ -13,35 +14,34 @@ const handleCarouselNavigation = () => {
   Array.from(slides).forEach((slide, index) => {
     slide.addEventListener("click", () => {
       pauseCarousel = true;
+      slideIndex = index;
       Array.from(carouselItems).forEach((item) => {
         item.style.transform = `translateX(-${index*100}%)`
       });
+      setInterval(() => pauseCarousel = false, 2000);
     })
   });
 }
 
 const runCarousel = () => {
-  let i = 1;
-  
   const carouselItems = document.querySelectorAll(".carousel_item"); 
   setInterval(() => {
     const runCarousel = !carouselHovered && !pauseCarousel;
     if (runCarousel) {
       Array.from(carouselItems).forEach((item) => {
-        if(i < carouselItems.length){
+        if(slideIndex < carouselItems.length){
           console.log(item);
-          item.style.transform = `translateX(-${i*100}%)`
+          item.style.transform = `translateX(-${slideIndex*100}%)`
         }
       });
-       
-      if(i < carouselItems.length){
-        i++;
+      if(slideIndex < carouselItems.length){
+        slideIndex++;
       }
       else{
-        i=0;
+        slideIndex=0;
       }
     }
-  }, 10000)
+  }, 3000)
   
 }
 
