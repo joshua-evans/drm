939f157617e4c3658e0bb4cc3a0877d56897e127
joshua-evans <evans.joshua.d@gmail.com>
2025-08-16 12:54:16 -0400

    fixed carousel, header links, upcoming links

--- js/carousel.js
+++ js/carousel.js
@@ -1,19 +1,61 @@
-/*const changeCssCarouselClass = () => {
-  const snappers = document.getElementsByClassName("carousel__snapper");  
-  console.log(snappers);
-  for(let i = 0; i < snappers.length; i++) {
-    snappers[i].className = "carousel__snapper__rev";
-  }
-}*/
-function jump(){
-  //var url = location.href;               //Save down the URL without hash.
-  location.href = "#carousel__slide1";                 //Go to the target element.
-  //history.replaceState(null,null,url);   //Don't like hashes. Changing it back.
+let carouselHovered = false;
+let pauseCarousel = false;
+
+const showCarouselNavigation = () => {
+  const jsCarouselNavigation = document.getElementById("js_nav");
+  jsCarouselNavigation.style.display = "block";
 }
 
-const goToFirstSlide = () => {
-  setInterval(jump, 7000);
+const handleCarouselNavigation = () => {
+  const slides = document.getElementsByClassName("carousel__navigation-item");
+  const carouselItems = document.querySelectorAll(".carousel_item");
+
+  Array.from(slides).forEach((slide, index) => {
+    slide.addEventListener("click", () => {
+      pauseCarousel = true;
+      Array.from(carouselItems).forEach((item) => {
+        item.style.transform = `translateX(-${index*100}%)`
+      });
+    })
+  });
 }
-goToFirstSlide();
-//changeCssCarouselClass();
-//jsCarousel();
+
+const runCarousel = () => {
+  let i = 1;
+  
+  const carouselItems = document.querySelectorAll(".carousel_item"); 
+  setInterval(() => {
+    const runCarousel = !carouselHovered && !pauseCarousel;
+    if (runCarousel) {
+      Array.from(carouselItems).forEach((item) => {
+        if(i < carouselItems.length){
+          console.log(item);
+          item.style.transform = `translateX(-${i*100}%)`
+        }
+      });
+       
+      if(i < carouselItems.length){
+        i++;
+      }
+      else{
+        i=0;
+      }
+    }
+  }, 10000)
+  
+}
+
+const stopCarouselOnHover = () => {
+  const carousel = document.getElementById("carousel");
+  carousel.addEventListener("mouseover", () => {
+    carouselHovered = true;
+  });
+  carousel.addEventListener("mouseout", () => {
+    carouselHovered = false;
+  });
+}
+
+showCarouselNavigation()
+runCarousel();
+stopCarouselOnHover();
+handleCarouselNavigation();
